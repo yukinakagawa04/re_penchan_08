0aec9a3fb875d5c879383252a76eb07b6ec45fb6
yukinakagawa04 <9704yn@gmail.com>
2023-01-13 00:09:30 +0900

    re_penchan

--- 2_top.php
+++ 2_top.php
@@ -15,7 +15,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-    <title>確認画面！</title>
+    <title>確認画面</title>
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link href="https://unpkg.com/sanitize.css" rel="stylesheet" />
     <link rel="stylesheet" href="css/style.css">
